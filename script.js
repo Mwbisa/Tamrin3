9c7e3d2e21a566ebb82295beb9ef2586796f5b12
Mobina <Mobinaabdollahi20@gmail.com>
2024-08-02 22:12:35 +0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const fullName="Mobina Abdollahi";
-
-const space=fullName.indexOf(" ");
-const firstName=fullName.slice(0,space);
-console.log("Firstname : "+firstName)
-
-const del=fullName.replace(firstName,"Lastname :");
-console.log(del)
